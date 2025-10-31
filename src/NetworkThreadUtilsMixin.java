6c4d55994d5af90a688fbad9d5bebbf2b671d3e1
q12323 <jacha1227@gmail.com>
2025-10-31 22:30:44 +0900

    initial commit

---
+++ src/NetworkThreadUtilsMixin.java
@@ -0,0 +1,20 @@
+package sapv.terminalsolver.mixin;
+
+import net.minecraft.network.NetworkThreadUtils;
+import net.minecraft.network.listener.PacketListener;
+import net.minecraft.network.packet.Packet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetworkThreadUtils.class)
+public class NetworkThreadUtilsMixin {
+    @Inject(
+            method = "method_11072(Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/network/packet/Packet;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/Packet;apply(Lnet/minecraft/network/listener/PacketListener;)V")
+    )
+    private static <T extends PacketListener> void method_11072(T packetListener, Packet<T> packet, CallbackInfo ci) {
+        // main receive packet
+    }
+}
